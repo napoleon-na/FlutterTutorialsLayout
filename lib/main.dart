3da9038d287aa0066222d96261c39badf2b776b9
Naomi Watanabe <napoleon.na1120@gmail.com>
2018-08-18 21:37:40 +0900

    Step 5: Implement the image section

--- lib/main.dart
+++ lib/main.dart
@@ -90,6 +90,19 @@ Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situate
         primarySwatch: Colors.blue,
       ),
       home: new MyHomePage(title: 'Flutter Demo Home Page'),
+        body: ListView(
+          children: [
+            Image.asset(
+              'images/lake.jpg',
+              width: 600.0,
+              height: 240.0,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
+        ),
     );
   }
 }
