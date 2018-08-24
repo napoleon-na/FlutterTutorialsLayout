5325404400ce09965352bf70eb231b7f7134ddb0
Naomi Watanabe <napoleon.na1120@gmail.com>
2018-08-24 17:37:41 +0900

    Lay out multiple widgets vertically and horizontally - Common layout widgets : Container

--- lib/main.dart
+++ lib/main.dart
@@ -153,116 +153,179 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    var titleText = Container(
-      padding: EdgeInsets.all(20.0),
-      child: Text(
-        'Strawberry Pavlova',
-        style: TextStyle(
-          fontWeight: FontWeight.w800,
-          letterSpacing: 0.5,
-          fontSize: 30.0,
-        ),
+
+    var container = Container(
+      decoration: BoxDecoration(
+        color: Colors.black26,
       ),
-    );
-
-    var subTitle = Text(
-      '''
-Pavlova is a meringue-based dessert named after the Russian ballerina Anna Pavlova. Pavlova features a crisp crust and soft, light inside, topped with fruit and whipped cream.
-''',
-      textAlign: TextAlign.center,
-      style: TextStyle(
-        fontFamily: 'Georgia',
-        fontSize: 25.0,
-      ),
-    );
-
-    var ratings = Container(
-      padding: EdgeInsets.all(20.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Icon(Icons.star, color: Colors.black),
-              Icon(Icons.star, color: Colors.black),
-              Icon(Icons.star, color: Colors.black),
-              Icon(Icons.star, color: Colors.black),
-              Icon(Icons.star, color: Colors.black),
-            ],
-          ),
-          Text(
-            '170 Reviews',
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.w800,
-              fontFamily: 'Roboto',
-              letterSpacing: 0.5,
-              fontSize: 20.0
-            ),
-          ),
-        ],
-      ),
-    );
-
-    var descTextStyle = TextStyle(
-      color: Colors.black,
-      fontWeight: FontWeight.w800,
-      fontFamily: 'Roboto',
-      letterSpacing: 0.5,
-      fontSize: 18.0,
-      height: 2.0,
-    );
-
-    var iconList = DefaultTextStyle.merge(
-      style: descTextStyle,
-      child: Container(
-        padding: EdgeInsets.all(20.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Column(
-              children: [
-                Icon(Icons.kitchen, color: Colors.green[500]),
-                Text('PREP:'),
-                Text('25 min'),
-              ],
-            ),
-            Column(
-              children: [
-                Icon(Icons.timer, color: Colors.green[500]),
-                Text('COOK:'),
-                Text('1 hr'),
-              ],
-            ),
-            Column(
-              children: [
-                Icon(Icons.restaurant, color: Colors.green[500]),
-                Text('FEEDS:'),
-                Text('4-6'),
-              ],
-            ),
-          ],
-        ),
-      )
-    );
-
-    var leftColumn = Container(
-      padding: EdgeInsets.fromLTRB(20.0, 30.0, 20.0, 20.0),
       child: Column(
         children: [
-          titleText,
-          subTitle,
-          ratings,
-          iconList,
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 10.0, color: Colors.black38),
+                    borderRadius: 
+                        const BorderRadius.all(const Radius.circular(8.0)),
+                  ),
+                  margin: const EdgeInsets.all(4.0),
+                  child: Image.asset('images/pic7.jpg'),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 10.0, color: Colors.black38),
+                    borderRadius: 
+                        const BorderRadius.all(const Radius.circular(8.0)),
+                  ),
+                  margin: const EdgeInsets.all(4.0),
+                  child: Image.asset('images/pic8.jpg'),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 10.0, color: Colors.black38),
+                    borderRadius: 
+                        const BorderRadius.all(const Radius.circular(8.0)),
+                  ),
+                  margin: const EdgeInsets.all(4.0),
+                  child: Image.asset('images/pic9.jpg'),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 10.0, color: Colors.black38),
+                    borderRadius: 
+                        const BorderRadius.all(const Radius.circular(8.0)),
+                  ),
+                  margin: const EdgeInsets.all(4.0),
+                  child: Image.asset('images/pic10.jpg'),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
 
-    var mainImage = Image.asset(
-      'images/pavlova.jpg',
-      fit: BoxFit.cover,
-    );
+//     var titleText = Container(
+//       padding: EdgeInsets.all(20.0),
+//       child: Text(
+//         'Strawberry Pavlova',
+//         style: TextStyle(
+//           fontWeight: FontWeight.w800,
+//           letterSpacing: 0.5,
+//           fontSize: 30.0,
+//         ),
+//       ),
+//     );
+
+//     var subTitle = Text(
+//       '''
+// Pavlova is a meringue-based dessert named after the Russian ballerina Anna Pavlova. Pavlova features a crisp crust and soft, light inside, topped with fruit and whipped cream.
+// ''',
+//       textAlign: TextAlign.center,
+//       style: TextStyle(
+//         fontFamily: 'Georgia',
+//         fontSize: 25.0,
+//       ),
+//     );
+
+//     var ratings = Container(
+//       padding: EdgeInsets.all(20.0),
+//       child: Row(
+//         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//         children: [
+//           Row(
+//             mainAxisSize: MainAxisSize.min,
+//             children: [
+//               Icon(Icons.star, color: Colors.black),
+//               Icon(Icons.star, color: Colors.black),
+//               Icon(Icons.star, color: Colors.black),
+//               Icon(Icons.star, color: Colors.black),
+//               Icon(Icons.star, color: Colors.black),
+//             ],
+//           ),
+//           Text(
+//             '170 Reviews',
+//             style: TextStyle(
+//               color: Colors.black,
+//               fontWeight: FontWeight.w800,
+//               fontFamily: 'Roboto',
+//               letterSpacing: 0.5,
+//               fontSize: 20.0
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+
+//     var descTextStyle = TextStyle(
+//       color: Colors.black,
+//       fontWeight: FontWeight.w800,
+//       fontFamily: 'Roboto',
+//       letterSpacing: 0.5,
+//       fontSize: 18.0,
+//       height: 2.0,
+//     );
+
+//     var iconList = DefaultTextStyle.merge(
+//       style: descTextStyle,
+//       child: Container(
+//         padding: EdgeInsets.all(20.0),
+//         child: Row(
+//           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//           children: [
+//             Column(
+//               children: [
+//                 Icon(Icons.kitchen, color: Colors.green[500]),
+//                 Text('PREP:'),
+//                 Text('25 min'),
+//               ],
+//             ),
+//             Column(
+//               children: [
+//                 Icon(Icons.timer, color: Colors.green[500]),
+//                 Text('COOK:'),
+//                 Text('1 hr'),
+//               ],
+//             ),
+//             Column(
+//               children: [
+//                 Icon(Icons.restaurant, color: Colors.green[500]),
+//                 Text('FEEDS:'),
+//                 Text('4-6'),
+//               ],
+//             ),
+//           ],
+//         ),
+//       )
+//     );
+
+    // var leftColumn = Container(
+    //   padding: EdgeInsets.fromLTRB(20.0, 30.0, 20.0, 20.0),
+    //   child: Column(
+    //     children: [
+    //       titleText,
+    //       subTitle,
+    //       ratings,
+    //       iconList,
+    //     ],
+    //   ),
+    // );
+
+    // var mainImage = Image.asset(
+    //   'images/pavlova.jpg',
+    //   fit: BoxFit.cover,
+    // );
 
     // var packedRow = Row(
     //   mainAxisSize: MainAxisSize.min,
@@ -279,22 +342,23 @@ Pavlova is a meringue-based dessert named after the Russian ballerina Anna Pavlo
         title: Text(widget.title),
       ),
       body: Center(
-        child: Container(
-          margin: EdgeInsets.fromLTRB(0.0, 40.0, 0.0, 30.0),
-          height: 600.0,
-          child: Card(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  width: 440.0,
-                  child: leftColumn,
-                ),
-                mainImage,
-              ]
-            ),
-          ),
-        ),
+        child: container,
+        // child: Container(
+        //   margin: EdgeInsets.fromLTRB(0.0, 40.0, 0.0, 30.0),
+        //   height: 600.0,
+        //   child: Card(
+        //     child: Row(
+        //       crossAxisAlignment: CrossAxisAlignment.start,
+        //       children: [
+        //         Container(
+        //           width: 440.0,
+        //           child: leftColumn,
+        //         ),
+        //         mainImage,
+        //       ]
+        //     ),
+        //   ),
+        // ),
         // child: packedRow,
         // child: Row(
         //   crossAxisAlignment: CrossAxisAlignment.center,
